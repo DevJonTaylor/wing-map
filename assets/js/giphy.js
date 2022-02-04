12ae17bd143ef3ae4ed0682b1c3ea7dfbdeac67a
JoDamaso <darryldamaso@gmail.com>
2022-02-04 17:48:23 -0600

    fetched

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,5 +1,7 @@
 // API key for Giphy stored in const
 const apiKey = "CJcLxlB9BUsGBRRe4GWIXJofKRRNvPUR";
+// Also need a 
+// I need a container to connect the giphy generated picture, whenever the user selects a choice. 
 var test = document.getElementById("vs");
 
 //randomCorrect has the parameters for a random gif that is revelent to the word `correct`
@@ -9,11 +11,12 @@ function displayPicture (correct) {
     let apiUrl = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${correct?"correct":"wrong"}&rating=g`;
 
     fetch(apiUrl)
+    // 
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        // console.log(data)
+        console.log(data)
         var picture = data.data.images.downsized.url;
         var correct = document.createElement('img');
         correct.setAttribute("src", picture);
