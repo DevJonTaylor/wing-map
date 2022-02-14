bc1bf84808ee722e15c6ab99cd94657c029410ec
kbugz <kbugusky@gmail.com>
2022-02-13 22:05:00 -0600

    Make sure I committed correctly earlier

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -9,46 +9,46 @@ var testBox2 = document.getElementById("box2");
 // need to add more js/function to check if the answer was wrong
 // figure out a way to get a different image without refreshing 
 
-function displayPicture (correct) {
-    let apiUrl = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${correct?"yay":"no"}&rating=r`;
+function displayPicture(correct) {
+    let apiUrl = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${correct ? "yay" : "no"}&rating=r`;
 
     fetch(apiUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        // console.log(data)
-        var picture = data.data.images.original.url;
-        giphyModal.querySelector('img').setAttribute("src", picture);
-        // console.log(picture);
-    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            // console.log(data)
+            var picture = data.data.images.original.url;
+            giphyModal.querySelector('img').setAttribute("src", picture);
+            // console.log(picture);
+        })
 
-    // if (
+        // if (
 
-    // )
-    .catch(function (error) {
-        alert('no picture');
-        console.log(error);
-    });
+        // )
+        .catch(function (error) {
+            alert('no picture');
+            console.log(error);
+        });
 };
 
-function displayPictureWrong () {
+function displayPictureWrong() {
     let apiUrl = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag="no"}&rating=r`;
 
     fetch(apiUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        // console.log(data)
-        var picture = data.data.images.original.url;
-        giphyModal.querySelector('img').setAttribute("src", picture);
-        // console.log(picture);
-    })
-    .catch(function (error) {
-        alert('no picture');
-        console.log(error);
-    });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            // console.log(data)
+            var picture = data.data.images.original.url;
+            giphyModal.querySelector('img').setAttribute("src", picture);
+            // console.log(picture);
+        })
+        .catch(function (error) {
+            alert('no picture');
+            console.log(error);
+        });
 };
 
 
@@ -57,54 +57,54 @@ function displayPictureWrong () {
 
 // Function for our modals to work
 document.addEventListener('DOMContentLoaded', () => {
-// Functions to open and close a modal
-function openModal($el) {
-    $el.classList.add('is-active');
-    
-}
+    // Functions to open and close a modal
+    function openModal($el) {
+        $el.classList.add('is-active');
 
-function closeModal($el) {
-    $el.classList.remove('is-active');
-}
+    }
 
-function closeAllModals() {
-    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
-    closeModal($modal);
+    function closeModal($el) {
+        $el.classList.remove('is-active');
+    }
+
+    function closeAllModals() {
+        (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+            closeModal($modal);
+        });
+    }
+
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger, i, arr) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+
+        $trigger.addEventListener('click', () => {
+            if ($trigger.children[0].matches("#box2") || $trigger.children[0].matches("#box3") || $trigger.children[0].matches("#box4")) {
+                displayPicture(true);
+                openModal($target);
+            } else {
+                openModal($target);
+            }
+        });
     });
-}
 
-// Add a click event on buttons to open a specific modal
-(document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger, i, arr) => {
-    const modal = $trigger.dataset.target;
-    const $target = document.getElementById(modal);
-    
-    $trigger.addEventListener('click', () => {
-        if ($trigger.children[0].matches("#box2") || $trigger.children[0].matches("#box3") || $trigger.children[0].matches("#box4")) {
-            displayPicture(true);
-            openModal($target);
-        } else {
-            openModal($target);
+    // Add a click event on various child elements to close the parent modal
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+        const $target = $close.closest('.modal');
+
+        $close.addEventListener('click', () => {
+            closeModal($target);
+        });
+    });
+
+    // Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+        const e = event || window.event;
+
+        if (e.keyCode === 27) { // Escape key
+            closeAllModals();
         }
     });
-});
-
-// Add a click event on various child elements to close the parent modal
-(document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-    const $target = $close.closest('.modal');
-
-    $close.addEventListener('click', () => {
-    closeModal($target);
-    });
-});
-
-// Add a keyboard event to close all modals
-document.addEventListener('keydown', (event) => {
-    const e = event || window.event;
-
-    if (e.keyCode === 27) { // Escape key
-    closeAllModals();
-    }
-});
 });
 
 
