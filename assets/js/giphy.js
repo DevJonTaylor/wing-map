000eb47cbf4eea8b64df0f2fcacea823ce9fb84d
dominique <domin@DESKTOP-OT2687Q>
2022-02-14 00:48:20 -0600

    dev merge

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -7,12 +7,15 @@ var testBox2 = document.getElementById("box2");
 
 // the arrays could hold what the cpu says I think it should be displayed in the modal 
 var correctTalk = ["CPU: beep boo bap... Lucky guess", "CPU: beep boo bap... I guess humans are smart", "CPU: beep boo bap...Looks I need better players", "CPU: beep boo bap...Nice one!", "CPU: beep boo bap...Maybe you're the mvp", "CPU: beep boo bap...Maybe I'll get the next round", "CPU: beep boo bap... Not fair I've never seen a football game", "CPU: beep boo bap...I promise I'm usally better","CPU: beep boo bap...Jon Taylor is that you?"]
-var  trashTalk = ["CPU: beep boo bap...I've seen the browns with more wins", "CPU: beep boo bap...Looks like you took the browns to the superbowl", "CPU: beep boo bap...Turn over on downs", "CPU: beep boo bap...Wow smooth just like your brain", "CPU: beep boo bap...Crazy your stats are just as bad as your player choice :)", "CPU: beep boo bap...I.AM.SASS-BOT ... YOU.ARE.TRASH-BOT", "CPU: beep boo bap...My creator is smarter than you and he does'nt get functions","CPU: beep boo bap...You're more of a cub than a Bear", "CPU: beep boo bap...You smell -'John the mack Damaso'"]
+var trashTalk = ["CPU: beep boo bap...I've seen the browns with more wins", "CPU: beep boo bap...Looks like you took the browns to the superbowl", "CPU: beep boo bap...Turn over on downs", "CPU: beep boo bap...Wow smooth just like your brain", "CPU: beep boo bap...Crazy your stats are just as bad as your player choice :)", "CPU: beep boo bap...I.AM.SASS-BOT ... YOU.ARE.TRASH-BOT", "CPU: beep boo bap...My creator is smarter than you and he does'nt get functions","CPU: beep boo bap...You're more of a cub than a Bear", "CPU: beep boo bap...You smell -'John the mack Damaso'"]
 var gameWin = ["CPU: beep boo bap...You win!","CPU: beep boo bap...Here's your virtual ring", "CPU: beep boo bap... Congrats your brain must have a wrinkle or 2", "CPU: beep boo bap... Double or nothing"]
-var gameFalse = ["CPU: beep boo bap...Grab your bags nerd game over", "CPU: beep boo bap...I have more cores than you have brain cells don't feel bad","CPU: beep boo bap... check the mirror for a brainless animal", "CPU: beep boo bap... The point is to win bud"]
+var gameLost = ["CPU: beep boo bap...Grab your bags nerd game over", "CPU: beep boo bap...I have more cores than you have brain cells don't feel bad","CPU: beep boo bap... check the mirror for a brainless animal", "CPU: beep boo bap... The point is to win bud"]
 // currently, always targeting the `correct` parameter in the API url
 // need to add more js/function to check if the answer was wrong
 // figure out a way to get a different image without refreshing 
+
+console.log(correctTalk.length, trashTalk.length,gameWin.length,gameLost.length)
+
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
   }
